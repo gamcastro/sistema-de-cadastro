883d3ceafbe77fc88f14541831373ee541a0c9cb
George Castro <gamcastro@bol.com.br>
2023-03-23 21:37:47 -0300

    adicionando mais um console log

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console.log de testes
 console.log("George fez esse commit sem vcs verem.")
+console.log("Enviando para o GitHub")
+
+
